d423e0b415a407b9cb5f9a0df0dfcec7e86202f7
JR Lawhorne <jr@fieldstone.io>
2021-05-07 17:42:17 -0400

    More work on the home page. It's much better. Needs some sprucing up by Jonathan.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,111 +14,157 @@
     ">
         <div class="d-flex justify-content-center align-items-center w-100">
             <div class="text-white">
-                <h1 class="display-3">Operate Your HVAC Business as an Owner</h1>
-                <h4 class="lead">Price Books, Jobs &amp; Invoices</h4>
-                <a class="btn btn-primary btn-lg mt-3" href="#!" role="button">Join Now!</a>
+                <h1 class="display-3">Field Service Management Software</h1>
+                <h2 class="lead">for Heating, Cooling and Plumbing Business</h2>
+                <a class="btn btn-primary btn-lg mt-3" href="https://pro.fieldstone.io/register" role="button">Start Free Trial!</a>
             </div>
         </div>
     </div>
     <div class="d-md-flex flex-md-equal w-100 ps-md-3">
         <div class="bg-dark me-md-3 px-3 px-md-5 text-center text-white overflow-hidden">
             <div class="py-3">
-                <h2 class="display-5">Benefit A</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Operate more Efficiently</h2>
             </div>
         </div>
         <div class="bg-light me-md-3 px-3 px-md-5 text-center overflow-hidden">
             <div class="p-3">
-                <h2 class="display-5">Benefit B</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Increase Profits</h2>
             </div>
         </div>
         <div class="bg-dark me-md-3 px-3 px-md-5 text-center text-white overflow-hidden">
             <div class="py-3">
-                <h2 class="display-5">Benefit C</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Reduce Stress</h2>
             </div>
         </div>
     </div>
-
-
-
 
     <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-        <div class="col-md-5 p-lg-5 mx-auto my-5">
-            <h1 class="display-4 fw-normal">Punny headline</h1>
-            <p class="lead fw-normal">And an even wittier subheading to boot. Jumpstart your marketing efforts with this example based on Apple’s marketing pages.</p>
-            <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+        <div class="col-md-8 p-lg-5 mx-auto my-5">
+            <h1 class="h2 fw-normal text-uppercase">Owners leave thousands of dollars on the table every year</h1>
+            <p class="lead fw-normal">How much business do you lose because you can't stay organized? How much profit
+                do you lose because your pricing is outdated? How many good employees leave because the stress at your
+                company is so high?</p>
+            <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
         </div>
-        <div class="product-device shadow-sm d-none d-md-block"></div>
-        <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+        <div class="product-device shadow-sm d-none d-lg-block"></div>
+        <div class="product-device product-device-2 shadow-sm d-none d-lg-block"></div>
     </div>
 
-    <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+    <div class="d-md-flex flex-md-equal w-100 ps-md-3">
+        <div class="bg-dark me-md-3 px-3 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+                <h1 class="h2 fw-normal">Save time and take control of your business with Fieldstone</h1>
+            </div>
+        </div>
+    </div>
+    <div class="d-md-flex flex-md-equal w-100 ps-md-3">
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <h2 class="display-5">Works Everywhere</h2>
+                <p class="lead">In the office or on the go, the Fieldstone cloud and mobile apps are ready to work for you.</p>
+            </div>
+        </div>
         <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
             <div class="my-3 py-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Clear Financials</h2>
+                <p class="lead">Mystery pricing is a thing of the past. When you use Fieldstone, you know the numbers for every job.</p>
+                <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
             </div>
-            <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
         </div>
         <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
             <div class="my-3 p-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Coordinated Team</h2>
+                <p class="lead">Coordinating a mobile team is complicated. Keep everyone on the same page with Fieldstone.</p>
             </div>
-            <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
         </div>
     </div>
 
-    <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-8 p-lg-5 mx-auto my-5">
+            <h1 class="h2 fw-normal text-uppercase">We feel your pain and we're here to help.</h1>
+            <p class="lead fw-normal">We work closely with a network of early adopters working side by side with them
+            to get their businesses organized and ensure they're profitable.</p>
+            <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
+        </div>
+        <div class="product-device shadow-sm d-none d-lg-block"></div>
+        <div class="product-device product-device-2 shadow-sm d-none d-lg-block"></div>
+    </div>
+
+    <div class="d-md-flex flex-md-equal w-100 ps-md-3">
         <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
             <div class="my-3 p-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Configure Your Account</h2>
+                <p class="lead">Configure Fieldstone to fit your business to get it ready for your team. We will show
+                you how and help if you need us.</p>
             </div>
-            <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
         </div>
-        <div class="bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+        <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
             <div class="my-3 py-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">Train Your Staff</h2>
+                <p class="lead">Video training and live webinars help prepare your team to go live and put Fieldstone
+                to work for your business.</p>
+                <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
             </div>
-            <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        </div>
+        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
+            <div class="my-3 p-3">
+                <h2 class="display-5">Go Live</h2>
+                <p class="lead">Be more efficient and more profitable with less stress.</p>
+            </div>
         </div>
     </div>
 
-    <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
-        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div class="my-3 p-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
-            </div>
-            <div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
+        <div class="col-md-8 p-lg-5 mx-auto my-5">
+            <h1 class="h2 fw-normal text-uppercase">Explanatory Paragraph</h1>
+            <p class="lead fw-normal">
+                At Fieldstone, we know that you want to be the leader of a profitable and successful business. In order
+                to do that, you need to know your numbers and keep your team coordinated. When you don’t, you end up
+                leaving thousands of dollars in profits on the table every year which makes you feel unsuccessful,
+                average, and defeated. We believe keeping your business operating at peak efficiency and maximum
+                profitability can be achieved with the help of our software. We understand that operating a field
+                service business with so many moving parts can be frustrating and difficult to manage which is why
+                we work closely with early adopters of our products to ensure they meet the highest operational
+                standards and exceed the expectations of our customers. When you choose to work with Fieldstone,
+                we start you off with a free trial that lets you experience the software without risk for 30 days.
+                This period gives us plenty of time to help you configure Fieldstone to fit your needs. Once Fieldstone
+                is configured for your business, we build confidence in your team with video training resources they
+                use to learn the features of Fieldstone. We also host live webinars where your team can join in to learn
+                and ask questions. Once your team is trained, you go live and start relying on Fieldstone for your daily
+                operations. So, sign up for a free trial to get started. We’re excited to see your success! In the
+                meantime, try our free loaded labor rate calculator. It may shock you to find out your true profitability
+                so you can stop underbidding jobs and instead earn what you’re worth.
+            </p>
+            <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
         </div>
-        <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div class="my-3 py-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
-            </div>
-            <div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
-        </div>
+        <div class="product-device shadow-sm d-none d-lg-block"></div>
+        <div class="product-device product-device-2 shadow-sm d-none d-lg-block"></div>
     </div>
 
-    <div class="d-md-flex flex-md-equal w-100 my-md-3 ps-md-3">
+    <div class="d-md-flex flex-md-equal w-100 ps-md-3">
         <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
             <div class="my-3 p-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
+                <h2 class="display-5">30 Day Free Trial</h2>
+                <p class="lead">Try Fieldstone risk free for 30 days.</p>
+                <p class="lead">Free</p>
+                <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
             </div>
-            <div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
+        </div>
+        <div class="bg-dark me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+            <div class="my-3 py-3">
+                <h2 class="display-5">Price Books</h2>
+                <p class="lead">Full featured flat rate price books for your whole team.</p>
+                <p class="lead">Coming Soon!</p>
+                <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
+            </div>
         </div>
         <div class="bg-light me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden">
-            <div class="my-3 py-3">
-                <h2 class="display-5">Another headline</h2>
-                <p class="lead">And an even wittier subheading.</p>
+            <div class="my-3 p-3">
+                <h2 class="display-5">Job Management</h2>
+                <p class="lead">Describe Manage Jobs Plan</p>
+                <p class="lead">Coming Soon!</p>
+                <a class="btn btn-lg btn-primary mt-3" href="https://pro.fieldstone.io/register">Start Free Trial!</a>
             </div>
-            <div class="bg-body shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div>
         </div>
     </div>
 @stop
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,14 @@
 <header class="site-header sticky-top py-2 px-5" style="height: 75px;">
-    <nav class="navbar navbar-expand-lg navbar-light fixed-top px-5" style="height: 75px;">
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top px-3 px-md-5" style="height: 75px;">
         {{--Logo--}}
-        <a class="navbar-brand" href="{{ url('/') }}">
-            <img src="{{ url('img/logo.min.svg') }}" alt="Fieldstone Logo" height="45"/>
+        <a class="navbar-brand d-inline-block" style="height:100%; width:18em;" href="{{ url('/') }}">
+            <img class="img-fluid my-auto" src="{{ url('img/logo.min.svg') }}" alt="Fieldstone Logo" />
         </a>
 
         <ul class="navbar-nav ml-auto">
             <li class="nav-item">
                 <a class="btn btn-link" href="https://pro.fieldstone.io/login">Login</a>
-                <a class="btn btn-lg btn-primary ml-5" href="https://pro.fieldstone.io/register">Join Now!</a>
+                <a class="btn btn-lg btn-primary ml-3 ml-md-5" href="https://pro.fieldstone.io/register">Free Trial!</a>
             </li>
         </ul>
     </nav>
